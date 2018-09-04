5077106da4a231dda1d3a7c13a157289d06cdb86
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-04 08:18:32 -0600

    added create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,20 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new(books_params)
+ 
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+ 
+  private
+ 
+    def books_params
+      params.require(:book).permit(:title, :author, :availible)
+    end
 end
